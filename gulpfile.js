c52ebb9cb218ddc84691da7d0efbb8e2ffd8ab63
cyberbudy <cyberbudy@gmail.com>
2015-03-20 00:02:04 +0200

    Almost ready

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,9 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
     jade = require('gulp-jade'),
+    babel = require('gulp-babel'),
     connect = require('gulp-connect');
+    image = require('gulp-image')
 
 var dir = 'build/development';
 
@@ -27,148 +29,38 @@ gulp.task('jade', function() {
         }))
         .pipe(gulp.dest('./build/development/'))
         .pipe(connect.reload())
-        // .on('error', console.log)
+});
+
+
+gulp.task('css', function() {
+    return gulp.src('./src/stylus/**/*.styl')
+        .pipe(stylus({}))
+        .pipe(gulp.dest('./build/development/css/'))
+        .pipe(connect.reload())
+});
+
+
+gulp.task('js6-5', function() {
+    return gulp.src('./src/js/**/*.js')
+        .pipe(babel())
+        .pipe(gulp.dest('./build/development/js/'))
+        .pipe(connect.reload())
 })
 
-
-// gulp.task('watch', function() {
-
-// })
+gulp.task('image', function() {
+    return gulp.src('./src/files/img/*')
+        .pipe(image())
+        .pipe(gulp.dest('./build/development/files/img/'))
+        .pipe(connect.reload())
+});
 
 
 gulp.task('watch', function() {
     gulp.watch('./src/template/**/*.jade', ['jade']);
+    gulp.watch('./src/stylus/**/*.styl', ['css']);
+    gulp.watch('./src/js/**/*.js', ['js6-5']);
+    gulp.watch('./src/files/img/*.*', ['image']);
 });
 
-gulp.task('default', ['connect', 'watch', 'jade']);
+gulp.task('default', ['connect', 'watch', 'jade', 'css', 'image', 'js6-5']);
 
-
-// gulp.task('default', function() {
-//     gulp.run('jade');
-
-//     gulp.watch('./src/template/**/.jade', function(event){
-//         gulp.run('jade');
-//     });
-// });
-
-// var server = lr();
-
-// // compile stylus
-// gulp.task('stylus', function() {
-//     gulp.src('./assets/stylus/screen.styl')
-//         .pipe(stylus({
-//             use: ['nib']
-//         })) // собираем stylus
-//     .on('error', console.log) // Если есть ошибки, выводим и продолжаем
-//     .pipe(gulp.dest('./public/css/')) // записываем css
-//     .pipe(refresh(server)); // даем команду на перезагрузку css
-// });
-
-
-
-// // Собираем html из Jade
-
-// gulp.task('jade', function() {
-//     gulp.src(['./assets/template/*.jade', '!./assets/template/_*.jade'])
-//         .pipe(jade({
-//             pretty: true
-//         }))  // Собираем Jade только в папке ./assets/template/ исключая файлы с _*
-//         .on('error', console.log) // Если есть ошибки, выводим и продолжаем
-//     .pipe(gulp.dest('./public/')) // Записываем собранные файлы
-//     .pipe(refresh(server)); // даем команду на перезагрузку страницы
-// }); 
-
-
-
-// // Собираем JS
-// gulp.task('js', function() {
-//     gulp.src(['./assets/js/**/*.js', '!./assets/js/vendor/**/*.js'])
-//         .pipe(concat('index.js')) // Собираем все JS, кроме тех которые находятся в ./assets/js/vendor/**
-//         .pipe(gulp.dest('./public/js'))
-//         .pipe(refresh(server)); // даем команду на перезагрузку страницы
-// });
-
-
-
-// // Копируем и минимизируем изображения
-
-// // gulp.task('images', function() {
-// //     gulp.src('./assets/img/**/*')
-// //         .pipe(imagemin())
-// //         .pipe(gulp.dest('./public/img'))
-
-// // });
-
-
-
-// // Локальный сервер для разработки
-// gulp.task('lr-server', function() {
-//     server.listen(35729, function(err) {
-//         if (err) return console.log(err);
-//     });
-// });
-// // gulp.task('http-server', function() {
-// //     connect()
-// //         .use(require('connect-livereload')())
-// //         .use(connect.static('./public'))
-// //         .listen('5000');
-
-// //     console.log('Server listening on http://localhost:9000');
-// // });
-
-// // Запуск сервера разработки gulp watch
-// gulp.task('default', function() {
-//     // Предварительная сборка проекта
-//     gulp.run('lr-server', 'stylus', 'jade', 'js');
-
-
-//     // watches
-//     gulp.watch('assets/stylus/**/*.styl', function() {
-//         gulp.run('stylus');
-//     });
-//     gulp.watch('assets/template/**/*.jade', function() {
-//         gulp.run('jade');
-//     });
-//     // gulp.watch('assets/img/**/*', function() {
-//     //     gulp.run('images');
-//     // });
-//     gulp.watch('assets/js/**/*', function() {
-//         gulp.run('js');
-//     });
-
-//     // gulp.run('http-server');
-// });
-
-// gulp.task('build', function() {
-//     // css
-//     gulp.src('./assets/stylus/screen.styl')
-//         .pipe(stylus({
-//             use: ['nib']
-//         })) // собираем stylus
-//     .pipe(myth()) // добавляем префиксы - http://www.myth.io/
-//     .pipe(csso()) // минимизируем css
-//     .pipe(gulp.dest('./build/css/')) // записываем css
-
-//     // jade
-//     gulp.src(['./assets/template/*.jade', '!./assets/template/_*.jade'])
-//         .pipe(jade())
-//         .pipe(gulp.dest('./build/'))
-
-//     // js
-//     gulp.src(['./assets/js/**/*.js', '!./assets/js/vendor/**/*.js'])
-//         .pipe(concat('index.js'))
-//         .pipe(uglify())
-//         .pipe(gulp.dest('./build/js'));
-
-//     // image
-//     // gulp.src('./assets/img/**/*')
-//     //     .pipe(imagemin())
-//     //     .pipe(gulp.dest('./build/img'))
-
-// });
-// // gulp.task('watch', function() {
-// //     gulp.watch('resources/js/*.js', ['lint', 'scripts']);
-// //     gulp.watch('resources/stylus/*.style', ['stylus']);
-// // });
-
-// // gulp.task('default', ['lint', 'stylus', 'scripts', 'watch']);
